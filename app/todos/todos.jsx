72203f643bd890220b924615c33d059205e8ab91
Tiffany Chen <tchen@thoughtworks.com>
2016-10-22 14:37:39 -0400

    Extract addTodo to function.

--- app/todos/todos.jsx
+++ app/todos/todos.jsx
@@ -4,20 +4,28 @@ import store from './store';
 let nextTodoId = 0;
 
 class TodoApp extends Component {
+
+    addTodo(input) {
+        store.dispatch({
+            type: 'ADD_TODO',
+            text: this.input.value,
+            id: nextTodoId++
+        });
+        this.input.value = '';
+    }
+
+    constructor(props, context) {
+        super(props, context);
+        this.addTodo = this.addTodo.bind(this)
+    }
+
     render() {
         return (
             <div>
                 <input ref={ node => {
                     this.input = node;
                 }} />
-                <button onClick={() => {
-                    store.dispatch({
-                        type: 'ADD_TODO',
-                        text: this.input.value,
-                        id: nextTodoId++
-                    });
-                    this.input.value = '';
-                }}>
+                <button onClick={this.addTodo}>
                 Add Todo
                 </button>
                 <ul>
